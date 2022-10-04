3e8d257131ae10fe2d358941e44f5dcc01ee70d4
Sungho Noh <nightwing@naver.com>
2022-10-04 16:22:32 +0900

    더미 이미지 경로 수정

--- src/components/content/subContent/Gallery.tsx
+++ src/components/content/subContent/Gallery.tsx
@@ -16,13 +16,13 @@ const Gallery = () => {
       <div className="mt-40">
         <Carousel width="480px" showStatus={false} showThumbs={false}>
           <div>
-            <img src={`${process.env.PUBLIC_URL}/01.jpeg`} />
+            <img src="https://images.unsplash.com/photo-1664362597715-d6089e55ae89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80" />
           </div>
           <div>
-            <img src={`${process.env.PUBLIC_URL}/02.jpeg`} />
+            <img src="https://images.unsplash.com/photo-1664380619406-49883ee01a55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2338&q=80" />
           </div>
           <div>
-            <img src={`${process.env.PUBLIC_URL}/03.jpeg`} />
+            <img src="https://images.unsplash.com/photo-1664388519483-9fdcbdba13a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
           </div>
         </Carousel>
       </div>
